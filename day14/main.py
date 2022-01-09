3c1f5d430683a56ebda0b26cadc03b2bf7528054
tiaotiaook <shihuaok@gmail.com>
2022-01-09 16:37:03 +0800

    day14第一轮对比生成的num_2在下一轮变成了num_1，这里的没有想出来，以及为什么clear()调用不了

--- day14/main.py
+++ day14/main.py
@@ -1,41 +1,50 @@
 from game_data import data
 import random
+from replit import clear
 
+def check_k(answer, a, b):
+    if answer == "a":
+        if a > b:
+            return True
+        else:
+            return False
+    elif answer == "b":
+        if a > b:
+            return False
+        else:
+            return True
 
-
-num_1 = random.choice(data)
-print(num_1)
-print(f"compare A: {num_1['name']} , a {num_1['description']}, from {num_1['country']}.")
-print("VS")
+score = 0
+game_continue = True
 num_2 = random.choice(data)
-print(num_2)
-print(f"compare B: {num_2['name']} , a {num_2['description']}, from {num_2['country']}.")
 
 
+while game_continue:
+    num_1 = num_2
+    num_1_follower = num_1['follower_count']
+    print(num_1_follower)
+    print(f"compare A: {num_1['name']} , a {num_1['description']}, from {num_1['country']}.")
 
-def compare(answer,a,b):
-        if answer == "A":
-            if num_1['follower_count'] > num_2['follower_count']:
-                score += 1
-                print(f"you are right ,current score is {score}")
-                return False
-            else:
-                print(f"that is wrong. your final score: {score}")
-                return True
-        elif answer == "B":
-            if num_1['follower_count'] > num_2['follower_count']:
-                print(f"that is wrong. your final score: {score}")
-                return True
-            else:
-                score += 1
-                print(f"you are right ,current score is {score}")
-                return False
+    print("VS")
+
+    num_2 = random.choice(data)
+    num_2_follower = num_2['follower_count']
+    print ( num_2_follower )
+    print ( f"compare B: {num_2['name']} , a {num_2['description']}, from {num_2['country']}." )
 
 
-compare(num_1,num_2)
-end_of_game = False
-while not compare(num_1,num_2):
-    num_3 = random.choice(data)
+    answer = input("who has more followers? type 'a' or 'b' : ").lower()
 
+    is_correct = check_k(answer,num_1_follower,num_2_follower)
+
+    clear()
+
+    if is_correct:
+        score += 1
+        print(f"you are right. your score is {score}")
+
+    else:
+        game_continue = False
+        print(f"you are wrong.your score is {score}")
 
 
