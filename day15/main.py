4d2b5c22d95d700177950b259f08609ebbd1e6d2
tiaotiaook <shihuaok@gmail.com>
2022-01-12 23:42:43 +0800

    day15 还差亿点点

--- day15/main.py
+++ day15/main.py
@@ -3,6 +3,7 @@ MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk": 0,
             "coffee": 18,
         },
         "cost": 1.5,
@@ -30,20 +31,39 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+def money():
+    print("please insert coins")
+    quarters = int(input("how many quarters ? "))
+    dimes = int(input("how many dimes ? "))
+    nickles = int(input("how many nickles ? "))
+    pennies = int(input("how many pennies ? "))
 
-drink = input("what would you like ? (espresso/latte/cappuccino) : ")
-print(MENU[drink]['cost'])
+    total = round(quarters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01,2)
 
-print("please insert coins")
-quarters = int(input("how many quarters ? "))
-dimes = int(input("how many dimes ? "))
-nickles = int(input("how many nickles ? "))
-pennies = int(input("how many pennies ? "))
+    print(total)
 
-total = round(quarters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01,2)
+    change = total - MENU[drink]['cost']
+    print(f"here is your change {change}")
 
-print(total)
 
-change = total - MENU[drink]['cost']
-print(f"here is your change {change}")
+def last(drink):
 
+    water_last = resources['water'] - MENU[drink]['ingredients']['water']
+    print(f"water last {water_last}")
+    milk_last = resources['milk'] - MENU[drink]['ingredients']['milk']
+    print(f"milk last {milk_last}")
+    coffee_last = resources['coffee'] - MENU[drink]['ingredients']['coffee']
+    print(f"coffee last {coffee_last}")
+
+
+go_on = False
+while not go_on:
+    drink = input("what would you like ? (espresso/latte/cappuccino) : ")
+    print(MENU[drink]['cost'])
+
+    last(drink)
+
+    # if water_last < MENU[drink]['ingredients']['water'] or milk_last < MENU[drink]['ingredients']['milk'] or coffee_last < MENU[drink]['ingredients']['coffee']:
+    #     print("sorry")
+
+    money()
