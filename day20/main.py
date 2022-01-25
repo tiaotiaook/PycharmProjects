3a65de5d9fe95509613cc75f4a6a39d1c882a667
tiaotiaook <shihuaok@gmail.com>
2022-01-26 01:19:25 +0800

    day 20 snake move: if heading up also means can't heading down

--- day20/main.py
+++ day20/main.py
@@ -1,5 +1,6 @@
 
 from turtle import Screen,Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,35 +9,23 @@ screen.bgcolor("black")
 screen.title("my snake game")
 screen.tracer(0)
 
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
 
-# segment_1 = Turtle("square")
-# segment_1.color("white")
-#
-# segment_2 = Turtle("square")
-# segment_2.color("white")
-# segment_2.goto(-20,0)
-#
-# segment_3 = Turtle("square")
-# segment_3.color("white")
-# segment_3.goto(-40,0)
 
-starting_position = [(0,0),(-20,0),(-40,0)]
 
-segment_list = []
 
-for position in starting_position:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segment_list.append(new_segment)
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(1)
-    for seg in segment_list:
-        seg.forward(20)
+    time.sleep(0.1)
+
+    snake.move()
 
 
 
@@ -49,3 +38,17 @@ while game_is_on:
 
 
 screen.exitonclick()
+
+
+
+
+# segment_1 = Turtle("square")
+# segment_1.color("white")
+#
+# segment_2 = Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20,0)
+#
+# segment_3 = Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40,0)
