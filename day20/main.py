01e43833eb02ec02f8a71ae28574b2aa139983b6
tiaotiaook <shihuaok@gmail.com>
2022-01-27 23:53:52 +0800

    snake game scoreboard & extend & die & slice

--- day20/main.py
+++ day20/main.py
@@ -2,6 +2,7 @@
 from turtle import Screen,Turtle
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -33,6 +35,23 @@ while game_is_on:
 
     if snake.head.distance(food)<15:
         food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+
+
+    if snake.head.xcor() > 295 or snake.head.xcor() < -295 \
+            or snake.head.ycor() > 295 or snake.head.ycor() < -295:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+
 
 
 
