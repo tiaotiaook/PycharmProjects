0ee0424c6129749206d8b423389903fe8267ae42
tiaotiaook <shihuaok@gmail.com>
2022-01-29 23:34:58 +0800

    day 22 pong game over : ball move & scoreboard

--- day22/main.py
+++ day22/main.py
@@ -1,4 +1,8 @@
 from turtle import Turtle,Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -8,35 +12,36 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
-
-
-paddle = Turtle()
-paddle.shape("square")
-paddle.shapesize(stretch_wid=5,stretch_len=1)
-paddle.color("white")
-paddle.penup()
-paddle.goto(350,0)
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
+ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.listen()
-screen.onkey(go_up,"Up")
-screen.onkey(go_down,"Down")
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down,"Down")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
 
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.distance(r_paddle)<50 and ball.xcor()>320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor()>380:
+        ball.reset_position()
+
+    if ball.xcor()<-380:
+        ball.reset_position()
+
+
+
 
 
 
