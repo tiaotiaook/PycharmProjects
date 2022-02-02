b0ab63347625ec1ccd1ae878bafd479bcb8befab
tiaotiaook <shihuaok@gmail.com>
2022-02-02 23:27:37 +0800

    day24 elative and absolute file paths

--- day24/main.py
+++ day24/main.py
@@ -1,10 +1,10 @@
-# with open("my_file.txt") as gg:
-#     contents = gg.read()
-#     print(contents)
+with open("../../Desktop/my_file.txt") as gg:
+    contents = gg.read()
+    print(contents)
 
 # with open("my_file.txt",mode = "a") as file:
 #     file.write("\nkjgj")
 
 
-with open("new_file.txt",mode = "w") as file:
-    file.write("new kjgj")
+# with open("new_file.txt",mode = "w") as file:
+#     file.write("new kjgj")
