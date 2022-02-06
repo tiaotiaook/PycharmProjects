65cac7c2fb65a56e837b90a7a6cee0a9df61eea2
tiaotiaook <shihuaok@gmail.com>
2022-02-06 23:53:49 +0800

    day 26: convert a sentence into a list of words ➡️ x = txt.split() & new_dict = {new_key:new_value for (key,value) in dict.item if test}

--- day26/Exercise.py
+++ day26/Exercise.py
@@ -15,8 +15,47 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 #Write your 1 line code 👇 below:
 
-result = [num for num in numbers if num%2==0]
+# result = [num for num in numbers if num%2==0]
 
 #Write your code 👆 above:
 
-print(result)
+# print(result)
+
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+# new_dict = {new_key:new_value for (key,value) in dict.item if test}
+# Write your code below:
+
+# convert a sentence into a list of words
+dictory = sentence.split()
+print(dictory)
+
+new_dict = {word: len(word) for word in dictory}
+
+print(new_dict)
+#
+# names = ["jsgd","suigdu","shd","shgdkbcjkw","sjgc","hs","hfhhj"]
+# students_scores = {student:random.randint(1,100) for student in names}
+# print(students_scores)
+# passed_student = {student: score for (student,score) in students_scores.items() if score > 60}
+# print(passed_student)
+
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+
+
+# Write your code 👇 below:
+
+weather_f = {xingqi : float((temp*9/5)+32) for (xingqi,temp) in weather_c.items()}
+
+print(weather_f)
--- day26/main.py
+++ day26/main.py
@@ -1,20 +1,29 @@
-numbers = [1,2,3]
-new_list = [n+1 for n in numbers]
-print(new_list)
+import random
+
+# numbers = [1,2,3]
+# new_list = [n+1 for n in numbers]
+# print(new_list)
 
 # new_list = [new_item for item in list]
 
-name = "Angela"
-new_list  = [letter for letter in name]
-print(new_list)
-
-new_number = [ 2*num  for num in range(1,5)]
-print(new_number)
+# name = "Angela"
+# new_list  = [letter for letter in name]
+# print(new_list)
+#
+# new_number = [ 2*num  for num in range(1,5)]
+# print(new_number)
 
 # new_list = [new_item for item in list if test]
 
 names = ["jsgd","suigdu","shd","shgdkbcjkw","sjgc","hs","hfhhj"]
 # short_names = [name for name in names if len(name)<5 ]
 # print(short_names)
-long_names = [name.upper() for name in names if len(name)>5]
-print(long_names)
+# long_names = [name.upper() for name in names if len(name)>5]
+# print(long_names)
+
+# new_dict = {new_key:new_value for (key,value) in dict.item if test}
+names = ["jsgd","suigdu","shd","shgdkbcjkw","sjgc","hs","hfhhj"]
+students_scores = {student:random.randint(1,100) for student in names}
+print(students_scores)
+passed_student = {student: score for (student,score) in students_scores.items() if score > 60}
+print(passed_student)
