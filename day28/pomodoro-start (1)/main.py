f4217fc2d41bdf6e892234a3fa7badb6275ea61f
tiaotiaook <shihuaok@gmail.com>
2022-02-11 18:55:52 +0800

    day 28 dynamic typing
     [if count_sec == 0:    count_sec = "00"]

---
+++ day28/pomodoro-start (1)/main.py
@@ -0,0 +1,64 @@
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down ( 5*60 )
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+
+    count_min = math.floor(count/60)
+    # 分钟 4.8只保留4，5.2只保留5
+    count_sec = count % 60
+    # 秒
+    if count_sec == 0:
+        count_sec = "00"
+
+
+    canvas.itemconfig(timer_text,text=f"{count_min} : {count_sec}")
+    if count>0:
+        window.after(1000,count_down,count-1)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100,pady=50,bg=YELLOW)
+
+
+label_time = Label(text = "Timer",fg = GREEN,bg= YELLOW,font=(FONT_NAME,50))
+label_time.grid(column = 1,row=0 )
+
+
+canvas = Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+
+timer_text = canvas.create_text(100, 130, text="00:00",fill = "white",font=(FONT_NAME,35,"bold"))
+canvas.grid(column = 1,row=1 )
+
+
+start_button = Button(text="Start",highlightthickness=0,command=start_timer)
+start_button.grid(column=0,row=2)
+
+
+reset_button = Button(text="Reset",highlightthickness=0)
+reset_button.grid(column=2,row=2)
+
+check_marks = Label(text = "✔",fg=GREEN,bg= YELLOW)
+check_marks.grid(column=1,row=3)
+
+window.mainloop()
