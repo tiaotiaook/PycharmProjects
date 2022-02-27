cdbea07570c137d159be37baac2afc276ef9cb47
tiaotiaook <shihuaok@gmail.com>
2022-02-27 23:37:30 +0800

    day 37 : Http post requests

--- day37/main.py
+++ day37/main.py
@@ -1,64 +1,45 @@
 import requests
-from datetime import datetime
 
-USERNAME="bibifei"
-TOKEN="kjgchgkgcghk"
+TOKEN = "jkgkjfjhfghdugk"
+USER_NAME = "shihua"
 GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-
     "token":TOKEN,
-    "username":USERNAME,
+    "username":USER_NAME,
     "agreeTermsOfService":"yes",
-    "notMinor":"yes",
+    "notMinor":"yes"
 }
 
-# response = requests.post(url=pixela_endpoint,json=user_params)
+# response = requests.post(url = pixela_endpoint,json = user_params)
 # print(response.text)
 
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_endpoint =f"{pixela_endpoint}/{USER_NAME}/graphs"
 
 graph_config = {
+
     "id":"GRAPH_ID",
-    "name":"Cycling Graph",
-    "unit":"Km",
+    "name":"meditation",
+    "unit":"mins",
     "type":"float",
-    "color":"ajisai"
+    "color":"shibafu"
 }
 
-headers = {
-    "X-USER-TOKEN": TOKEN
+headers = {"X-USER-TOKEN":TOKEN}
 
-}
-
-# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint,json = graph_config,headers=headers)
 # print(response.text)
 
-pixel_creation_endpoint= f"{pixela_endpoint}/{USERNAME}//graphs/{GRAPH_ID}"
+pixel_creation_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/{GRAPH_ID}"
 
-today = datetime(year=2021,month=12,day=14)
-# print(today.strftime("%Y%m%d"))
 
 pixel_data = {
-    "date":today.strftime("%Y%m%d"),
-    "quantity":input("how many kilometers did you cycle today ?"),
+    "date":"20220227",
+    "quantity":"15.8"
+
 }
 
-response = requests.post(url=pixel_creation_endpoint,json=pixel_data,headers=headers)
-print(response.text)
-
-update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
-
-
-new_pixel_data = {
-   "quantity":"4.5"
-}
-
-# response = requests.put(url=update_endpoint,json=new_pixel_data,headers=headers)
-# print(response.text)
-
-delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
-response = requests.delete(url = delete_endpoint,headers=headers)
+response = requests.post(url = pixel_creation_endpoint,json=pixel_data,headers=headers)
 print(response.text)
