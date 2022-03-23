df428ec9147dadd53ab71efffca470be60ff0eec
tiaotiaook <shihuaok@gmail.com>
2022-03-24 01:27:07 +0800

    day 48 fail to get the price from amazon🤔

--- day48/main.py
+++ day48/main.py
@@ -2,7 +2,14 @@ from selenium import webdriver
 
 chrome_driver_path = "/Users/2shihua1yin/Desktop/Web Development/chromedriver 2"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
-driver.get("https://www.amazon.com")
+driver.get("https://www.amazon.com/Instant-Pot-60-Max-Electric/dp/B077T9YGRM/ref=sr_1_4?crid=BBI0WCRRRR2P&keywords=instant+pot&qid=1648055163&sprefix=instant%2Caps%2C1498&sr=8-4")
+price = driver.find_element(by=By.Class,value="a-offscreen")
+# _by_class_name("a-offscreen")
+
+print(price.text)
+
+
+
 # driver.close()
-driver.quit()
+# driver.quit()
 
