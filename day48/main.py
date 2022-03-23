18b59836fd8f51c29a360555256d0995675bdd40
tiaotiaook <shihuaok@gmail.com>
2022-03-24 00:57:02 +0800

    day 48 driver.get()/driver.quit()

--- day48/main.py
+++ day48/main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+from selenium import webdriver
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+chrome_driver_path = "/Users/2shihua1yin/Desktop/Web Development/chromedriver 2"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get("https://www.amazon.com")
+# driver.close()
+driver.quit()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
