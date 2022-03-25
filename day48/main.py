893403a6dac7817bdf160187d7fc5583cc52422d
tiaotiaook <shihuaok@gmail.com>
2022-03-25 23:57:15 +0800

    day 48 Use Selenium to Scrape Website Data

--- day48/main.py
+++ day48/main.py
@@ -4,11 +4,18 @@ from selenium.webdriver.common.by import By
 chrome_driver_path = "/Users/2shihua1yin/Desktop/Web Development/chromedriver 2"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-
 driver.get("https://www.python.org/")
 
-link = driver.find_element(By.CSS_SELECTOR,".documentation-widget a")
-print(link)
+
+driver.find_elements_by_css_selector()
+
+#
+# bug_link = driver.find_element(By.XPATH,'//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+# print(bug_link.text)
+
+
+# link = driver.find_element(By.CSS_SELECTOR,".documentation-widget a")
+# print(link.text)
 
 # logo = driver.find_element(By.CLASS_NAME,"python-logo")
 # print(logo.size)
@@ -25,5 +32,5 @@ print(link)
 
 
 # driver.close()
-driver.quit()
+# driver.quit()
 
