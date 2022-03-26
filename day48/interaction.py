c0cf7ffaad07dadff7308638269af2f6d1420ef1
tiaotiaook <shihuaok@gmail.com>
2022-03-26 21:04:29 +0800

    day 48 Automate Filling Out Forms and Clicking Buttons with Selenium

---
+++ day48/interaction.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+chrome_driver_path = "/Users/2shihua1yin/Desktop/Web Development/chromedriver 2"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+
+driver.get("http://secure-retreat-92358.herokuapp.com/")
+
+first_name = driver.find_element(By.NAME,"fName")
+first_name.send_keys("tulong")
+last_name = driver.find_element(By.NAME,"lName")
+last_name.send_keys("shaonv")
+email = driver.find_element(By.NAME,"email")
+email.send_keys("tulongshaonv@gail.com")
+
+submit = driver.find_element(By.CSS_SELECTOR,"form button")
+submit.click()
+
+# driver.get("https://en.wikipedia.org/wiki/Main_Page")
+
+# article_count = driver.find_element(By.CSS_SELECTOR,"#articlecount a")
+# article_count .click()
+
+# all_portals = driver.find_element(By.LINK_TEXT,"All portals")
+# all_portals.click()
+
+# search = driver.find_element(By.NAME,"search")
+# search.send_keys("Python")
+# search.send_keys(Keys.ENTER)
+
+# driver.quit()
--- day48/main.py
+++ day48/main.py
@@ -7,7 +7,22 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://www.python.org/")
 
 
-driver.find_elements_by_css_selector()
+event_times = driver.find_elements(By.CSS_SELECTOR,".event-widget time")
+# for time in event_times:
+#     print(time.text)
+event_names = driver.find_elements(By.CSS_SELECTOR,".event-widget li a")
+events = {}
+
+for n in range(len(event_times)):
+    events[n]={
+        "time":event_times[n].text,
+        "name":event_names[n].text,
+    }
+
+print(events)
+
+
+driver.quit()
 
 #
 # bug_link = driver.find_element(By.XPATH,'//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
@@ -32,5 +47,5 @@ driver.find_elements_by_css_selector()
 
 
 # driver.close()
-# driver.quit()
+
 
