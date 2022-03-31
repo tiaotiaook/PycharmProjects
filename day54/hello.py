078e94160f9d4f3a156319dd0ead3e0928c097ea
tiaotiaook <shihuaok@gmail.com>
2022-03-31 23:55:53 +0800

    day 55 Flask URL Paths

--- day54/hello.py
+++ day54/hello.py
@@ -1,12 +1,40 @@
 from flask import Flask
 app = Flask(__name__)
 
-print(__name__)
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 
-if __name__ == "main":
-    app.run()
+
+@app.route("/bye")
+def bye():
+    return "Bye!"
+
+
+@app.route("/username/<name>")
+def greet(name):
+    return f"Hello {name}!"
+
+
+if __name__ == "__main":
+    app.run(debug=True)
+
+
+
+
+
+
+
+# from flask import Flask
+# app = Flask(__name__)
+#
+# print(__name__)
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+#
+#
+# if __name__ == "main":
+#     app.run()
