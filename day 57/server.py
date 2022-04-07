3ab93d1fb826b5892918e5b9ad84e19ab2c4e00b
tiaotiaook <shihuaok@gmail.com>
2022-04-07 23:31:17 +0800

    day 57 over and tomorrow will be a big day

--- day 57/server.py
+++ day 57/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -13,7 +14,15 @@ def home():
 
 @app.route("/guess/<name>")
 def guess(name):
-    return render_template("guess.html",person_name=name)
+    gender_url = f"https://api.genderize.io?name={name}"
+    gender_response=requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+    age_url = f"https://api.agify.io?name={name}"
+    age_response=requests.get(age_url)
+    age_data = age_response.json()
+    age=age_data["age"]
+    return render_template("guess.html", person_name=name,gender=gender)
 
 if __name__ == "__main__":
     app.run(debug=True)
