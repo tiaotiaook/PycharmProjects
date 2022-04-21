ee0eebcef732ec510a14613ec3c6bddf122449ee
tiaotiaook <shihuaok@gmail.com>
2022-04-21 23:22:37 +0800

    day 55 review Flask URL Paths

--- day 54 review/hello.py
+++ day 54 review/hello.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="text-align: center">Hello, World!<h1>'
+
+
+@app.route("/bye")
+def say_bye():
+    return "Bye"
+
+
+@app.route("/username/<name>/<int:number>")
+def greet(name,number):
+    return f"Hello there {name}, you are {number} years old!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
