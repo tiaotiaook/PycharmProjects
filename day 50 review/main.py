d5d9634743e2a524c33b45e80eb28c6b8ef0f388
tiaotiaook <shihuaok@gmail.com>
2022-05-04 22:12:16 +0800

    day 50 review beautiful soup + selenium webdriver 在酒店预订网站上爬取地址、链接、价格，再填入Google sheet 中，但是header是什么意思？？？

--- day 50 review/main.py
+++ day 50 review/main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 import time
+from selenium.webdriver.common.by import By
 
 header = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
@@ -14,7 +15,6 @@ data = response.text
 soup = BeautifulSoup(data, "html.parser")
 
 all_link_elements = soup.select(".list-card-top a")
-
 all_links = []
 for link in all_link_elements:
     href = link["href"]
@@ -24,4 +24,50 @@ for link in all_link_elements:
     else:
         all_links.append(href)
 
-print(all_links)
+# print(all_links)
+# print(len(all_links))
+
+all_address_elements =soup.select(".list-card-addr")
+all_address = []
+for address in all_address_elements:
+    all_address.append(address.get_text().split(" | ")[-1])
+
+# print(all_address)
+# print(len(all_address))
+
+all_price_elements = soup.select(".list-card-heading")
+all_prices = []
+for element in all_price_elements:
+    # Get the prices. Single and multiple listings have different tag & class structures
+    try:
+        # Price with only one listing
+        price = element.select(".list-card-price")[0].contents[0]
+    except IndexError:
+        # print('Multiple listings for the card')
+        # # Price with multiple listings
+        # price = element.select(".list-card-details li")[0].contents[0]
+        pass
+    finally:
+        all_prices.append(price)
+
+# print(all_prices)
+# print(len(all_prices))
+
+chrome_driver_path = "/Users/2shihua1yin/Desktop/Web Development/chromedriver 2"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+for n in range(len(all_links)):
+
+    driver.get("https://docs.google.com/forms/d/e/1FAIpQLSckArxIABU2pbcGNx6dmk2tZN5H2txoKRf24Vod2zSi8ze1bw/viewform?usp=sf_link")
+    time.sleep ( 2 )
+    address = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input' )
+    price = driver.find_element(By.XPATH,'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input' )
+    link = driver.find_element(By.XPATH,'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input' )
+    submit_button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div' )
+
+
+    address.send_keys(all_address[n])
+    price.send_keys ( all_prices[n] )
+    link.send_keys ( all_links[n] )
+    submit_button.click ()
+
