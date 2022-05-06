26f7eb86812b5e73489f35aeb8f56812daf4905c
tiaotiaook <shihuaok@gmail.com>
2022-05-06 23:30:26 +0800

    day 33 review response不需要para参数

--- day 33 review/main.py
+++ day 33 review/main.py
@@ -1,18 +1,43 @@
 import requests
+#
+# # reponses = requests.get(url="http://api.open-notify.org/iss-now.json")
+# #
+# # print(reponses.status_code)
+# # # 打印的是回应代码
+# #
+# # data = reponses.json()
+# # print(data)
+# #
+# # # 用json的格式获取数据
+# #
+# # longitude = reponses.json()["iss_position"]["longitude"]
+# # latitude = reponses.json()["iss_position"]["latitude"]
+# #
+# # # 创建元组
+# # iss_position = (longitude,latitude)
+# # print(iss_position)
+#
+MY_LAT = 51.507351
+MY_LONG = -0.127758
 
-reponses = requests.get(url="http://api.open-notify.org/iss-now.json")
+parameters = {
+    "lat":MY_LAT,
+    "lng":MY_LONG,
+}
 
-print(reponses.status_code)
-# 打印的是回应代码
+response = requests.get(url="https://api.sunrise-sunset.org/json")
+response.raise_for_status()
 
-data = reponses.json()
+data = response.json()
 print(data)
 
-# 用json的格式获取数据
-
-longitude = reponses.json()["iss_position"]["longitude"]
-latitude = reponses.json()["iss_position"]["latitude"]
-
-# 创建元组
-iss_position = (longitude,latitude)
-print(iss_position)
+# import requests
+# from datetime import datetime
+#
+# MY_LAT = 51.507351 # Your latitude
+# MY_LONG = -0.127758 # Your longitude
+#
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+# data = response.json()
+# print(data)
