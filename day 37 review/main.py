b70ac990ec39cacfe7369000f19966cc280c6e5b
tiaotiaook <shihuaok@gmail.com>
2022-05-10 23:29:34 +0800

    day 37 cool !!! create a Habit Tracking use pixela , three methods requests.post()  requests.put()   requests.delete()

--- day 37 review/main.py
+++ day 37 review/main.py
@@ -1,9 +1,11 @@
 
 import requests
+from datetime import datetime
 
 # requests.get()  requests.post()  requests.put()   requests.delete()
 USERNAME="tiaotiaotiao"
 TOKEN="sjdbjkgrk"
+GRAPH_ID ="tiaotiaodawang"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -35,4 +37,52 @@ headers ={
     "X-USER-TOKEN":TOKEN
 }
 
-response = requests.post(url=graph_endpoint,json=graph_config)
+# response = requests.post(url=graph_endpoint,json=graph_config,headers=headers)
+
+
+
+# 打卡
+
+post_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+# 倒入时间模块，但是需要整理post需要的格式  strftime
+
+today= datetime.now()
+
+
+post_config = {
+    "date":today.strftime("%Y%m%d"),
+    "quantity":"22.5",
+}
+
+
+# response = requests.post(url=post_endpoint,json=post_config,headers=headers)
+
+
+
+# update pixela
+
+update_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+
+update_config = {
+    "quantity": "0.5",
+}
+
+
+# response = requests.put(url=update_endpoint,json=update_config,headers=headers)
+# print(response.text)
+
+
+# delete pixela
+
+delete_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+
+# update_config = {
+#     "quantity": "0.5",
+# }
+
+
+response = requests.delete(url=delete_endpoint,headers=headers)
+print(response.text)
