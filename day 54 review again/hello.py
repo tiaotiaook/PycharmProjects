3da7a8ec07ac76438e65f482e562e4a8c4e2a619
tiaotiaook <shihuaok@gmail.com>
2022-05-17 23:48:07 +0800

    端口被占用，更换端口的另一个解决办法：if __name__ == "__main__":     app.run(port =7000)

---
+++ day 54 review again/hello.py
@@ -0,0 +1,17 @@
+# client sever database
+
+from flask import Flask
+app = Flask(__name__)
+
+print(__name__)
+
+
+
+@app.route('/')
+
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == "__main__":
+    app.run(port =7000)
