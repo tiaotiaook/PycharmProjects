9cb390d7baf82fc9d100be7dba15365d96f75d70
tiaotiaook <shihuaok@gmail.com>
2022-05-18 00:32:01 +0800

    day 54 review again 发现了端口占用的另一个解决方法，不用在terminal每次输入了，大概了解了@app.route('/')的意思

--- day 54 review again/hello.py
+++ day 54 review again/hello.py
@@ -1,16 +1,17 @@
 # client sever database
 
 from flask import Flask
+
 app = Flask(__name__)
 
-print(__name__)
-
-
-
 @app.route('/')
-
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/bye')
+def say_bye():
+    return 'bye'
 
 
 if __name__ == "__main__":
