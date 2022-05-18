241ec04b623833fa32a25320adcfa0af92b8045d
tiaotiaook <shihuaok@gmail.com>
2022-05-18 18:37:27 +0800

    day 55 review ：456 Advanced Decorators with *args and **kwargs

--- day 54 review again/hello.py
+++ day 54 review again/hello.py
@@ -6,13 +6,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="text-align:center">Hello, World!</h1>' \
+           '<p>This is a paragraph</p>' \
+           '<img src="https://media2.giphy.com/media/YRVP7mapl24G6RNkwJ/giphy.gif?cid=ecf05e474erpefiw9cduks3jqhjmqwr2btm0415dagp31my8&rid=giphy.gif&ct=g">'
 
 
 @app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
+
 def say_bye():
     return 'bye'
 
+# add variable sections to a URL by marking sections with <variable_name>
+
+@app.route('/username/<name>/<int:number>')
+def greet(name,number):
+    return f"Hello {name}, you are {number} years old!"
+
 
 if __name__ == "__main__":
-    app.run(port =7000)
+    app.run(port =7000 ,debug=True)
