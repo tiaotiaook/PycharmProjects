6464fc9d599a64288cd564c46fc333c9965ac823
tiaotiaook <shihuaok@gmail.com>
2022-05-21 18:03:55 +0800

    day 57 review again 其他都可以了，final project 最后一个步骤没写出来，也是最关键的一步，有时间要再复习一下html的嵌套逻辑

--- day 57 review again/server.py
+++ day 57 review again/server.py
@@ -3,28 +3,22 @@ import random
 import datetime
 import requests
 
-# year = datetime.datetime.now()
-# year_1 =year.strftime("%Y")
-
-
-
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     random_number = random.randint(1,10)
-    current_year = datetime.datetime.now().year
-    return render_template("index.html",num=random_number,t=current_year)
+    current_year = datetime.datetime.now ().year
+    return render_template("index.html",num=random_number,year = current_year)
 
 
 @app.route('/guess/<name>')
 def guess(name):
-    gender_url=f"https://api.genderize.io?name={name}"
+    gender_url = f"https://api.genderize.io?name={name}"
     gender_response = requests.get(gender_url)
     gender_data = gender_response.json()
-    gender=gender_data["gender"]
+    gender = gender_data["gender"]
 
     age_url = f"https://api.agify.io?name={name}"
     age_response = requests.get(age_url)
@@ -32,18 +26,24 @@ def guess(name):
     age = age_data["age"]
 
 
-    return render_template("guess.html",name=name,gender=gender,age=age)
+    return render_template("guess.html",name = name,gender=gender,age=age)
 
 
-@app.route("/blog/<num>")
+@app.route('/blog/<num>')
 def get_blog(num):
     print(num)
-    blog_url="https://api.npoint.io/c790b4d5cab58020d391"
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
     response = requests.get(blog_url)
     all_posts = response.json()
+
     return render_template("blog.html", posts=all_posts)
 
 
+
+
+
 if __name__ == "__main__":
-    app.run(port =7001 ,debug=True)
+    app.run(port =7002,debug=True)
+
+
 
