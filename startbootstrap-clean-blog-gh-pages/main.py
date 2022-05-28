90c385b82d80f1585ba0f4cab19c5fe59c5aed43
tiaotiaook <shihuaok@gmail.com>
2022-05-28 21:39:55 +0800

    day 59 作业是day58 + day57 的复习，包括html的排版设计，以及jinjia网页跳转，day57@app.route("/post/<int:index>") 要再看一遍

--- startbootstrap-clean-blog-gh-pages/main.py
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+import requests
+
+posts = requests.get("https://api.npoint.io/d73032b597901cc8af49").json()
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,5 +14,29 @@ def get_all_posts():
 
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run(port =7000,debug=True)
+
