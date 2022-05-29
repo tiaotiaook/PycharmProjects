38d04a4de27caa338bafb4cdeef1c31099cf4736
tiaotiaook <shihuaok@gmail.com>
2022-05-29 22:55:55 +0800

    day 60 作业还是写不出来，但是能把老师代码调试出来- - make html form submit a request

---
+++ day-59-end/main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template,request
+import requests
+
+posts = requests.get("https://api.npoint.io/858439cf4ef9ca8d6a46").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact",methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(port = 8001,debug=True)
