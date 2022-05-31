03961c53e8247eb67428aac922318dfaf0e3368a
tiaotiaook <shihuaok@gmail.com>
2022-06-01 01:12:06 +0800

    day 66 卡住第一天 pycharm 加载数据库是乱码 没法推进

--- day 66 Starting+Files+-+cafe-api-start/main.py
+++ day 66 Starting+Files+-+cafe-api-start/main.py
@@ -1,10 +1,16 @@
+
+
+
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 app = Flask(__name__)
 
 ##Connect to Database
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -28,9 +34,30 @@ class Cafe(db.Model):
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
 
 ## HTTP GET - Read Record
+
+@app.route("/random")
+def get_random_cafe():
+    cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(cafes)
+    return jsonify(
+        cafe={
+            "id": random_cafe.id,
+            "name": random_cafe.name,
+            "map_url": random_cafe.map_url,
+            "img_url": random_cafe.img_url,
+            "location": random_cafe.location,
+            "seats": random_cafe.seats,
+            "has_toilet": random_cafe.has_toilet,
+            "has_wifi": random_cafe.has_wifi,
+            "has_sockets": random_cafe.has_sockets,
+            "can_take_calls": random_cafe.can_take_calls,
+            "coffee_price": random_cafe.coffee_price,
+        }
+    )
+
 
 ## HTTP POST - Create Record
 
@@ -40,4 +67,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port = 6001,debug=True)
