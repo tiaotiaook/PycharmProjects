7855f6df04db2f2266e139fa0a1663a274ab06fb
tiaotiaook <shihuaok@gmail.com>
2022-06-12 22:17:22 +0800

    day 72 Matplotlib 和Matlab好像啊

--- day 72/main.py
+++ day 72/main.py
@@ -2,3 +2,23 @@
 with open("QueryResults.csv") as f:
     for line in f:
         print(line)
+
+
+
+        
+
+# used .groupby() to explore the number of posts and entries per programming language
+#
+# converted strings to Datetime objects with to_datetime() for easier plotting
+#
+# reshaped our DataFrame by converting categories to columns using .pivot()
+#
+# used .count() and isna().values.any() to look for NaN values in our DataFrame, which we then replaced using .fillna()
+#
+# created (multiple) line charts using .plot() with a for-loop
+#
+# styled our charts by changing the size, the labels, and the upper and lower bounds of our axis.
+#
+# added a legend to tell apart which line is which by colour
+#
+# smoothed out our time-series observations with .rolling().mean() and plotted them to better identify trends over time.
